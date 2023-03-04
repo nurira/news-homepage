ca863e128c105ef4f80fe337e4ea52359026451c
nrira <ranuri@pm.me>
2023-03-04 11:22:41 -0500

    add keys for mapped elements

--- src/NewArticles.jsx
+++ src/NewArticles.jsx
@@ -6,29 +6,29 @@ export default function NewArticles({ articles }) {
     <Wrapper>
       <h2>New</h2>
       {articles.map(({ title, teaser }, idx) => (
-        <>
+        <React.Fragment key={idx}>
           <NewArticle>
             <h3>{title}</h3>
             <p>{teaser}</p>
           </NewArticle>
           {idx < articles.length - 1 && <Divider spacing={32} />}
-        </>
+        </React.Fragment>
       ))}
     </Wrapper>
   );
 }
 
 const Wrapper = styled.section`
+  color: var(--color-white);
+  background-color: var(--color-darkblue);
+  padding: 1rem 1.5rem;
+
   h2 {
     font-size: 2.2rem;
     font-weight: var(--font-weight-bold);
     color: var(--color-primary);
     padding-block: 1rem;
   }
-
-  color: var(--color-white);
-  background-color: var(--color-darkblue);
-  padding: 1rem 1.5rem;
 `;
 
 const NewArticle = styled.article`
