653e88c27537422883682caf99ec3d0b471c15c2
nrira <ranuri@pm.me>
2023-03-04 11:23:04 -0500

    craete top articles component

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,13 @@ import styled from "styled-components/macro";
 import Navbar from "./NavBar";
 import MainArticle from "./MainArticle";
 import NewArticles from "./NewArticles";
+import TopArticles from "./TopArticles";
 import Divider from "./Divider";
 
-const DATA = {
-  mainArticle: {
-    title: "The Bright Future of Web 3.0?",
-    teaser: `We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?`,
-    src: "/images/image-web-3-desktop.jpg",
-  },
-  newArticles: [
-    {
-      title: "Hydrogen VS Electric Cars",
-      teaser: "Will hydrogen-fueled cars ever catch up to EVs?",
-    },
-    {
-      title: "The Downsides of AI Artistry",
-      teaser:
-        "What are the possible adverse effects of on-demand AI image generation?",
-    },
-    {
-      title: "Is VC Funding Drying Up?",
-      teaser:
-        "Private funding by VC firms is down 50% YOY. We take a look at what that means.",
-    },
-  ],
-};
+import { DATA } from "./data";
 
 export default function App() {
-  const { mainArticle, newArticles } = DATA;
+  const { mainArticle, newArticles, topArticles } = DATA;
   return (
     <Wrapper>
       <Header>
@@ -44,20 +23,7 @@ export default function App() {
           <MainArticle {...mainArticle} />
         </MainArticleWrapper>
         <NewArticles articles={newArticles} />
-        <TopArticlesWrapper>
-          <TopArticle>
-            <img src="/images/image-retro-pcs.jpg" alt="" />
-            <h2>Reviving Retro PCs</h2>
-          </TopArticle>
-          <TopArticle>
-            <img src="/images/image-top-laptops.jpg" alt="" />
-            <h2>Top 10 Laptops of 2022</h2>
-          </TopArticle>
-          <TopArticle>
-            <img src="/images/image-gaming-growth.jpg" alt="" />
-            <h2>The Growth of Gaming</h2>
-          </TopArticle>
-        </TopArticlesWrapper>
+        <TopArticles articles={topArticles} />
       </Main>
     </Wrapper>
   );
@@ -91,11 +57,3 @@ const Main = styled.main`
 const MainArticleWrapper = styled.div`
   grid-area: main-article;
 `;
-
-const TopArticlesWrapper = styled.section`
-  grid-area: top-stories;
-  justify-content: space-between;
-  display: flex;
-`;
-
-const TopArticle = styled.article``;
---
+++ src/TopArticles.jsx
@@ -0,0 +1,52 @@
+import styled from "styled-components/macro";
+
+export default function TopArticles({ articles }) {
+  return (
+    <Wrapper>
+      {articles.map(({ title, teaser, src }, idx) => (
+        <TopArticle key={idx}>
+          <img src={src} alt="" />
+          <div>
+            <span>{`0${idx + 1}`}</span>
+            <h2>{title}</h2>
+            <p>{teaser}</p>
+          </div>
+        </TopArticle>
+      ))}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.section`
+  grid-area: top-stories;
+  justify-content: space-between;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-column-gap: 34px;
+`;
+
+const TopArticle = styled.article`
+  display: flex;
+  gap: 1.5rem;
+
+  img {
+    max-width: 110px;
+  }
+
+  h2 {
+    line-height: 2;
+    font-size: 1.3rem;
+    color: var(--color-darkblue);
+  }
+
+  p {
+    color: var(--color-grayblue-dark);
+    line-height: 1.8;
+  }
+
+  span {
+    color: var(--color-grayblue);
+    font-size: 2.2rem;
+    font-weight: var(--font-weight-bold);
+  }
+`;
