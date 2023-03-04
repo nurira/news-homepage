8e457d595894d68e9761f7eac1ca0a328dfdcd9b
nrira <ranuri@pm.me>
2023-03-04 11:22:49 -0500

    split data out into separate file

---
+++ src/data.jsx
@@ -0,0 +1,40 @@
+export const DATA = {
+  mainArticle: {
+    title: "The Bright Future of Web 3.0?",
+    teaser: `We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?`,
+    src: "/images/image-web-3-desktop.jpg",
+  },
+  newArticles: [
+    {
+      title: "Hydrogen VS Electric Cars",
+      teaser: "Will hydrogen-fueled cars ever catch up to EVs?",
+    },
+    {
+      title: "The Downsides of AI Artistry",
+      teaser:
+        "What are the possible adverse effects of on-demand AI image generation?",
+    },
+    {
+      title: "Is VC Funding Drying Up?",
+      teaser:
+        "Private funding by VC firms is down 50% YOY. We take a look at what that means.",
+    },
+  ],
+  topArticles: [
+    {
+      title: "Reviving Retro PCs",
+      teaser: "What happens when old PCs are given modern upgrades?",
+      src: "/images/image-retro-pcs.jpg",
+    },
+    {
+      title: "Top 10 Laptops of 2022",
+      teaser: "Our best picks for various needs and budgets.",
+      src: "/images/image-top-laptops.jpg",
+    },
+    {
+      title: "The Growth of Gaming",
+      teaser: "How the pandemic has sparked fresh opportunities.",
+      src: "/images/image-gaming-growth.jpg",
+    },
+  ],
+};
